6d65de66c3185f2bc56b1684a1df6817bad904ce
nemusou <ryubi.102395@gmail.com>
2019-11-30 23:57:44 +0900

    feat: WebViewが読み込むhtmlファイルを変えた

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import {WebView} from 'react-native-webview';
 import WebViewCanvas from './components/WebViewCanvas'
 import LocalHtmlView from './components/LocalHtmlView'
 
-const src = require('./assets/phina.html')
+// const src = require('./assets/animations/opening.html')
+const src = require('./assets/animations/opening.html')
 
 export default class App extends React.Component {
   render() {
